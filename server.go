3a960a9b4beaf63b0badd19a6dabe369213bd3ca
Derek Rodriguez <drod2169@gmail.com>
2024-08-25 09:37:38 -0400

    add env file

--- server.go
+++ server.go
@@ -3,11 +3,14 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
+	"fmt"
 	"log"
 	"math"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
@@ -25,9 +28,24 @@ type DuelRequest struct {
 var db *sql.DB
 
 func main() {
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Construct database connection string from environment variables
+	dbURL := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_SSLMODE"),
+	)
+
 	// Initialize database connection
-	var err error
-	db, err = sql.Open("postgres", "postgres://username:password@localhost/dishduel?sslmode=disable")
+	db, err = sql.Open("postgres", dbURL)
 	if err != nil {
 		log.Fatal(err)
 	}
